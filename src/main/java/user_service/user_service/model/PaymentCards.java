c78c5f9bea645c99586eee9ac608a65e5a3ad057
Aliaksei <aliaksei@gmail.com>
2026-05-12 23:13:33 +0300

    add PaymentCards entity

---
+++ src/main/java/user_service/user_service/model/PaymentCards.java
@@ -0,0 +1,36 @@
+package user_service.user_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payment_cards")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentCards {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
