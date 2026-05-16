9bf85ac6cc133b68924b21d54445d6ec3ba63af5
Aliaksei <aliaksei@gmail.com>
2026-05-16 09:23:15 +0300

    add DAO layer

---
+++ src/main/java/com/innowise/userservice/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.innowise.userservice.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.innowise.userservice.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    @Query("SELECT u FROM User u WHERE u.active = true")
+    Page<User> findAllActiveUsers(Pageable pageable);
+
+    @Query(
+            value = "SELECT * FROM users WHERE active = false",
+            nativeQuery = true
+    )
+    Page<User> findAllInactiveUsers(Pageable pageable);
+}
