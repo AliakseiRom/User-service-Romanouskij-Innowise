f50a9e7e5f4841e3bfdd4ffe5542ff460137a138
Aliaksei <aliaksei@gmail.com>
2026-05-17 23:13:36 +0300

    Add find by email method to user repository

--- src/main/java/com/innowise/userservice/repository/UserRepository.java
+++ src/main/java/com/innowise/userservice/repository/UserRepository.java
@@ -22,4 +22,6 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
             nativeQuery = true
     )
     Page<User> findAllInactiveUsers(Pageable pageable);
+
+    String findByEmail(String email);
 }
